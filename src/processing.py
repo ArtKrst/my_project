ba807a7bc8e1caf16918eb41f35e4e65aa3c5291
Artem <artkr@gmail.com>
2024-10-16 15:49:57 +0500

    init

---
+++ src/processing.py
@@ -0,0 +1,13 @@
+from typing import List, Dict
+
+def filter_by_state(data: List[Dict], state: str = 'EXECUTED') -> List[Dict]:
+    """
+    Фильтрует список словарей по значению ключа 'state'.
+    """
+    return [item for item in data if item.get('state') == state]
+
+def sort_by_date(data: List[Dict], descending: bool = True) -> List[Dict]:
+    """
+    Сортирует список словарей по дате.
+    """
+    return sorted(data, key=lambda x: x['date'], reverse=descending)
